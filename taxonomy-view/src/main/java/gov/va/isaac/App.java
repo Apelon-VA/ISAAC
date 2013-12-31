5d80414affb1b388c7a7e963e1e1af51ea35b68e
Owen Carlsen <ocarlsen@westcoastinformatics.com>
2013-12-31 12:21:34 -0800

    Moving loadDataStore from AppController to App.

--- taxonomy-view/src/main/java/gov/va/isaac/App.java
+++ taxonomy-view/src/main/java/gov/va/isaac/App.java
@@ -1,10 +1,14 @@
 package gov.va.isaac;
 
 import gov.va.isaac.dialog.ErrorDialog;
+import gov.va.isaac.util.WBUtility;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.net.URL;
 
 import javafx.application.Application;
+import javafx.concurrent.Task;
 import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -12,6 +16,7 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 import javafx.stage.WindowEvent;
 
+import org.ihtsdo.otf.tcc.datastore.BdbTerminologyStore;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -24,19 +29,20 @@ import com.sun.javafx.tk.Toolkit;
  */
 public class App extends Application {
 
-    private static final Logger LOG = LoggerFactory.getLogger(App.class);
+	private static final Logger LOG = LoggerFactory.getLogger(App.class);
 
-    private AppController controller;
-    private ErrorDialog errorDialog;
+	private AppController controller;
+	private ErrorDialog errorDialog;
+    private boolean shutdown = false;
+    private BdbTerminologyStore dataStore;
 
-    @Override
+	@Override
     public void start(Stage primaryStage) throws Exception {
 
         URL fxmlURL = this.getClass().getResource("App.fxml");
         FXMLLoader loader = new FXMLLoader(fxmlURL);
         Parent root = (Parent) loader.load();
         this.controller = loader.getController();
-        controller.setApp(this);
 
         primaryStage.setTitle("Taxonomy Viewer");
         primaryStage.setScene(new Scene(root));
@@ -47,41 +53,126 @@ public class App extends Application {
         primaryStage.setMinHeight(primaryStage.getHeight());
         primaryStage.setMinWidth(primaryStage.getWidth());
 
-        // Handle window close event.
+		// Handle window close event.
         primaryStage.setOnCloseRequest(new EventHandler<WindowEvent>() {
 
-            @Override
-            public void handle(WindowEvent event) {
-                shutdown();
-            }
-        });
+			@Override
+			public void handle(WindowEvent event) {
+				shutdown();
+			}
+		});
 
         // Reusable error dialog.
         this.errorDialog = new ErrorDialog(primaryStage);
+
+        // Kick off a thread to open the DB connection.
+        loadDataStore(System.getProperty(BdbTerminologyStore.BDB_LOCATION_PROPERTY));
     }
 
-    public void showErrorDialog(final String title, final String message, final String details) {
+	public void showErrorDialog(final String title, final String message, final String details) {
 
-        // Make sure in application thread.
-        Toolkit.getToolkit().checkFxUserThread();
+		// Make sure in application thread.
+		Toolkit.getToolkit().checkFxUserThread();
 
-        errorDialog.setVariables(title, message, details);
-        errorDialog.showAndWait();
+		errorDialog.setVariables(title, message, details);
+		errorDialog.showAndWait();
+	}
+
+    private void loadDataStore(final String bdbFolderName) {
+
+        // Do work in background.
+        Task<BdbTerminologyStore> task = new Task<BdbTerminologyStore>() {
+
+            @Override
+            protected BdbTerminologyStore call() throws Exception {
+                LOG.info("Opening Workbench database");
+                BdbTerminologyStore dataStore = getDataStore(bdbFolderName);
+                LOG.info("Finished opening Workbench database");
+
+                // Check if user shut down early.
+                if (shutdown) {
+                    dataStore.shutdown();
+                    return null;
+                }
+
+                return dataStore;
+            }
+
+            @Override
+            protected void succeeded() {
+                dataStore = this.getValue();
+
+                // Inject into dependent classes.
+                WBUtility.setDataStore(dataStore);
+                AppContext appContext = new AppContext(App.this, dataStore);
+                controller.setAppContext(appContext);
+            }
+
+            @Override
+            protected void failed() {
+                Throwable ex = getException();
+
+                // Display helpful dialog to users.
+                if (ex instanceof FileNotFoundException) {
+                    String title = "No Snomed Database";
+                    String message = "The Snomed Database was not found.";
+                    LOG.error(message, ex);
+                    String details = "Please download the file\n\n"
+                            + "https://mgr.servers.aceworkspace.net/apps/va-archiva/repository/all/org/ihtsdo/otf/tcc-test-data/3.0/"
+                            + "\n\nand unzip it into\n\n"
+                            + System.getProperty("user.dir")
+                            + "\n\nand then restart the editor.";
+                    showErrorDialog(title, message, details);
+                } else {
+                    String title = "Unexpected error connecting to workbench database";
+                    String msg = ex.getClass().getName();
+                    String details = ex.getMessage();
+                    LOG.error(title, ex);
+                    showErrorDialog(title, msg, details);
+                }
+            }
+        };
+
+        Thread t = new Thread(task, "SCT_DB_Open");
+        t.setDaemon(true);
+        t.start();
     }
 
-    private void shutdown() {
-        LOG.info("Shutting down");
-        try {
-            controller.shutdown();
-        } catch (Exception ex) {
-            String message = "Trouble shutting down";
-            LOG.warn(message, ex);
-            showErrorDialog("Oops!", message, ex.getMessage());
+    protected BdbTerminologyStore getDataStore(String bdbFolderName) throws Exception {
+
+        // Default value if null.
+        if (bdbFolderName == null) {
+            bdbFolderName = "berkeley-db";  // Hard-coded in BdbTerminologyStore.
         }
-        LOG.info("Finished shutting down");
 
-        //System.exit(0);
+        // Sanity check.
+        File bdbFolderPath = new File(bdbFolderName);
+        if (! bdbFolderPath.exists()) {
+            throw new FileNotFoundException(bdbFolderName);
+        }
+
+        return new BdbTerminologyStore();
     }
+
+	private void shutdown() {
+		LOG.info("Shutting down");
+        shutdown = true;
+
+		try {
+	        if (dataStore != null) {
+	            dataStore.shutdown();
+	        }
+	        if (controller != null) {
+	            controller.shutdown();
+	        }
+		} catch (Exception ex) {
+			String message = "Trouble shutting down";
+			LOG.warn(message, ex);
+			showErrorDialog("Oops!", message, ex.getMessage());
+		}
+
+		LOG.info("Finished shutting down");
+	}
 
     public static void main(String[] args) {
         Application.launch(args);
--- taxonomy-view/src/main/java/gov/va/isaac/AppController.java
+++ taxonomy-view/src/main/java/gov/va/isaac/AppController.java
@@ -2,18 +2,12 @@ package gov.va.isaac;
 
 import gov.va.isaac.gui.SctTreeItem;
 import gov.va.isaac.gui.SctTreeView;
-import gov.va.isaac.util.WBUtility;
-
-import java.io.File;
-import java.io.FileNotFoundException;
-
 import javafx.concurrent.Task;
 import javafx.fxml.FXML;
 import javafx.scene.layout.BorderPane;
 
 import org.ihtsdo.otf.tcc.api.coordinate.StandardViewCoordinates;
 import org.ihtsdo.otf.tcc.api.metadata.binding.Taxonomies;
-import org.ihtsdo.otf.tcc.datastore.BdbTerminologyStore;
 import org.ihtsdo.otf.tcc.ddo.concept.ConceptChronicleDdo;
 import org.ihtsdo.otf.tcc.ddo.fetchpolicy.RefexPolicy;
 import org.ihtsdo.otf.tcc.ddo.fetchpolicy.RelationshipPolicy;
@@ -28,126 +22,67 @@ import org.slf4j.LoggerFactory;
  */
 public class AppController {
 
-    private static final Logger LOG = LoggerFactory.getLogger(AppController.class);
+	private static final Logger LOG = LoggerFactory.getLogger(AppController.class);
 
-    @FXML
-    private BorderPane browserPane;
+	@FXML private BorderPane browserPane;
 
-    private App app;
-    private BdbTerminologyStore dataStore;
-    private SctTreeView sctTree;
-    private boolean shutdown = false;
+    private AppContext appContext;
+	private SctTreeView sctTree;
 
-    /**
-     * Called by the FXMLLoader when initialization is complete.
-     */
-    @FXML
-    public void initialize() {
-        // Kick off a thread to open the DB connection.
-        loadDataStore();
-    }
+	public void setAppContext(AppContext appContext) {
+        this.appContext = appContext;
 
-    public void setApp(App app) {
-        this.app = app;
-    }
+        // Kick off a thread to load the root concept.
+        loadSctTree(appContext);
+	}
 
-    public void shutdown() {
-        LOG.info("Shutting down");
-        shutdown = true;
+	public void shutdown() {
+		LOG.info("Shutting down");
 
-        if (dataStore != null) {
-            dataStore.shutdown();
-        }
+		SctTreeView.shutdown();
+		SctTreeItem.shutdown();
 
-        SctTreeView.shutdown();
-        SctTreeItem.shutdown();
+		LOG.info("Finished shutting down");
+	}
 
-        LOG.info("Finished shutting down");
-    }
+	private void loadSctTree(final AppContext appContext) {
 
-    private void loadDataStore() {
+		// Do work in background.
+		Task<ConceptChronicleDdo> task = new Task<ConceptChronicleDdo>() {
 
-        // Do work in background.
-        Task<ConceptChronicleDdo> task = new Task<ConceptChronicleDdo>() {
+			@Override
+			protected ConceptChronicleDdo call() throws Exception {
+				LOG.info("Loading root concept");
+				ConceptChronicleDdo rootConcept = appContext.getDataStore().getFxConcept(
+						Taxonomies.SNOMED.getUuids()[0],
+						StandardViewCoordinates.getSnomedInferredLatest(),
+						VersionPolicy.ACTIVE_VERSIONS,
+						RefexPolicy.REFEX_MEMBERS,
+						RelationshipPolicy.ORIGINATING_AND_DESTINATION_TAXONOMY_RELATIONSHIPS);
+                LOG.info("Finished loading root concept");
 
-            @Override
-            protected ConceptChronicleDdo call() throws Exception {
-                LOG.info("Opening Workbench database");
-                dataStore = getDataStore(System.getProperty(BdbTerminologyStore.BDB_LOCATION_PROPERTY));
-                LOG.info("Finished opening Workbench database");
+                return rootConcept;
+			}
 
-                // Check if user shut down early.
-                if (shutdown) {
-                    dataStore.shutdown();
-                    return null;
-                }
+			@Override
+			protected void succeeded() {
+				ConceptChronicleDdo result = this.getValue();
+				sctTree = new SctTreeView(appContext, result);
+				browserPane.setCenter(sctTree);
+			}
 
-                // Inject into dependent classes.
-                WBUtility.setDataStore(dataStore);
+			@Override
+			protected void failed() {
+				Throwable ex = getException();
+				String title = "Unexpected error loading root concept";
+				String msg = ex.getClass().getName();
+				LOG.error(title, ex);
+				AppController.this.appContext.getApp().showErrorDialog(title, msg, ex.getMessage());
+			}
+		};
 
-                return dataStore.getFxConcept(
-                        Taxonomies.SNOMED.getUuids()[0],
-                        StandardViewCoordinates.getSnomedInferredLatest(),
-                        VersionPolicy.ACTIVE_VERSIONS,
-                        RefexPolicy.REFEX_MEMBERS,
-                        RelationshipPolicy.ORIGINATING_AND_DESTINATION_TAXONOMY_RELATIONSHIPS);
-            }
-
-            @Override
-            protected void succeeded() {
-                ConceptChronicleDdo result = this.getValue();
-
-                // User could have shut down early.
-                if (result != null) {
-                    AppContext appContext = new AppContext(app, dataStore);
-                    sctTree = new SctTreeView(appContext, result);
-                    browserPane.setCenter(sctTree);
-                }
-            }
-
-            @Override
-            protected void failed() {
-                Throwable ex = getException();
-
-                // Display helpful dialog to users.
-                if (ex instanceof FileNotFoundException) {
-                    String title = "No Snomed Database";
-                    String message = "The Snomed Database was not found.";
-                    LOG.error(message, ex);
-                    String details = "Please download the file\n\n"
-                            + "https://mgr.servers.aceworkspace.net/apps/va-archiva/repository/all/org/ihtsdo/otf/tcc-test-data/3.0/"
-                            + "\n\nand unzip it into\n\n"
-                            + System.getProperty("user.dir")
-                            + "\n\nand then restart the editor.";
-                    app.showErrorDialog(title, message, details);
-                } else {
-                    String title = "Unexpected error connecting to workbench database";
-                    String msg = ex.getClass().getName();
-                    String details = ex.getMessage();
-                    LOG.error(title, ex);
-                    app.showErrorDialog(title, msg, details);
-                }
-            }
-        };
-
-        Thread t = new Thread(task, "SCT_DB_Open");
-        t.setDaemon(true);
-        t.start();
-    }
-
-    protected BdbTerminologyStore getDataStore(String bdbFolderName) throws Exception {
-
-        // Default value if null.
-        if (bdbFolderName == null) {
-            bdbFolderName = "berkeley-db";  // Hard-coded in BdbTerminologyStore.
-        }
-
-        // Sanity check.
-        File bdbFolderPath = new File(bdbFolderName);
-        if (! bdbFolderPath.exists()) {
-            throw new FileNotFoundException(bdbFolderName);
-        }
-
-        return new BdbTerminologyStore();
-    }
+		Thread t = new Thread(task, "Root_Concept_Load");
+		t.setDaemon(true);
+		t.start();
+	}
 }
