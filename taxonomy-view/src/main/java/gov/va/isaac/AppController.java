7f8544726d74cd49ab98233a86081ab51ce937b9
Owen Carlsen <ocarlsen@westcoastinformatics.com>
2013-12-30 17:12:32 -0800

    Replacing tabs with spaces.

--- taxonomy-view/src/main/java/gov/va/isaac/AppController.java
+++ taxonomy-view/src/main/java/gov/va/isaac/AppController.java
@@ -28,126 +28,126 @@ import org.slf4j.LoggerFactory;
  */
 public class AppController {
 
-	private static final Logger LOG = LoggerFactory.getLogger(AppController.class);
+    private static final Logger LOG = LoggerFactory.getLogger(AppController.class);
 
-	@FXML
-	private BorderPane browserPane;
+    @FXML
+    private BorderPane browserPane;
 
-	private App app;
-	private BdbTerminologyStore dataStore;
-	private SctTreeView sctTree;
-	private boolean shutdown = false;
+    private App app;
+    private BdbTerminologyStore dataStore;
+    private SctTreeView sctTree;
+    private boolean shutdown = false;
 
-	/**
-	 * Called by the FXMLLoader when initialization is complete.
-	 */
-	@FXML
-	public void initialize() {
-		// Kick off a thread to open the DB connection.
-		loadDataStore();
-	}
+    /**
+     * Called by the FXMLLoader when initialization is complete.
+     */
+    @FXML
+    public void initialize() {
+        // Kick off a thread to open the DB connection.
+        loadDataStore();
+    }
 
-	public void setApp(App app) {
-		this.app = app;
-	}
+    public void setApp(App app) {
+        this.app = app;
+    }
 
-	public void shutdown() {
-		LOG.info("Shutting down");
-		shutdown = true;
+    public void shutdown() {
+        LOG.info("Shutting down");
+        shutdown = true;
 
-		if (dataStore != null) {
-			dataStore.shutdown();
-		}
+        if (dataStore != null) {
+            dataStore.shutdown();
+        }
 
-		SctTreeView.shutdown();
-		SctTreeItem.shutdown();
+        SctTreeView.shutdown();
+        SctTreeItem.shutdown();
 
-		LOG.info("Finished shutting down");
-	}
+        LOG.info("Finished shutting down");
+    }
 
-	private void loadDataStore() {
+    private void loadDataStore() {
 
-		// Do work in background.
-		Task<ConceptChronicleDdo> task = new Task<ConceptChronicleDdo>() {
+        // Do work in background.
+        Task<ConceptChronicleDdo> task = new Task<ConceptChronicleDdo>() {
 
-			@Override
-			protected ConceptChronicleDdo call() throws Exception {
-				LOG.info("Opening Workbench database");
-				dataStore = getDataStore(System.getProperty(BdbTerminologyStore.BDB_LOCATION_PROPERTY));
-				LOG.info("Finished opening Workbench database");
+            @Override
+            protected ConceptChronicleDdo call() throws Exception {
+                LOG.info("Opening Workbench database");
+                dataStore = getDataStore(System.getProperty(BdbTerminologyStore.BDB_LOCATION_PROPERTY));
+                LOG.info("Finished opening Workbench database");
 
-				// Check if user shut down early.
-				if (shutdown) {
-					dataStore.shutdown();
-					return null;
-				}
+                // Check if user shut down early.
+                if (shutdown) {
+                    dataStore.shutdown();
+                    return null;
+                }
 
-				// Inject into dependent classes.
-				WBUtility.setDataStore(dataStore);
+                // Inject into dependent classes.
+                WBUtility.setDataStore(dataStore);
 
-				return dataStore.getFxConcept(
-						Taxonomies.SNOMED.getUuids()[0],
-						StandardViewCoordinates.getSnomedInferredLatest(),
-						VersionPolicy.ACTIVE_VERSIONS,
-						RefexPolicy.REFEX_MEMBERS,
-						RelationshipPolicy.ORIGINATING_AND_DESTINATION_TAXONOMY_RELATIONSHIPS);
-			}
+                return dataStore.getFxConcept(
+                        Taxonomies.SNOMED.getUuids()[0],
+                        StandardViewCoordinates.getSnomedInferredLatest(),
+                        VersionPolicy.ACTIVE_VERSIONS,
+                        RefexPolicy.REFEX_MEMBERS,
+                        RelationshipPolicy.ORIGINATING_AND_DESTINATION_TAXONOMY_RELATIONSHIPS);
+            }
 
-			@Override
-			protected void succeeded() {
-				ConceptChronicleDdo result = this.getValue();
+            @Override
+            protected void succeeded() {
+                ConceptChronicleDdo result = this.getValue();
 
-				// User could have shut down early.
-				if (result != null) {
-				    AppContext appContext = new AppContext(app, dataStore);
-					sctTree = new SctTreeView(appContext, result);
-					browserPane.setCenter(sctTree);
-				}
-			}
+                // User could have shut down early.
+                if (result != null) {
+                    AppContext appContext = new AppContext(app, dataStore);
+                    sctTree = new SctTreeView(appContext, result);
+                    browserPane.setCenter(sctTree);
+                }
+            }
 
-			@Override
-			protected void failed() {
-				Throwable ex = getException();
+            @Override
+            protected void failed() {
+                Throwable ex = getException();
 
-				// Display helpful dialog to users.
-				if (ex instanceof FileNotFoundException) {
-					String title = "No Snomed Database";
-					String message = "The Snomed Database was not found.";
-					LOG.error(message, ex);
-					String details = "Please download the file\n\n"
-							+ "https://mgr.servers.aceworkspace.net/apps/va-archiva/repository/all/org/ihtsdo/otf/tcc-test-data/3.0/"
-							+ "\n\nand unzip it into\n\n"
-							+ System.getProperty("user.dir")
-							+ "\n\nand then restart the editor.";
-					app.showErrorDialog(title, message, details);
-				} else {
-					String title = "Unexpected error connecting to workbench database";
-					String msg = ex.getClass().getName();
-					String details = ex.getMessage();
-					LOG.error(title, ex);
-					app.showErrorDialog(title, msg, details);
-				}
-			}
-		};
+                // Display helpful dialog to users.
+                if (ex instanceof FileNotFoundException) {
+                    String title = "No Snomed Database";
+                    String message = "The Snomed Database was not found.";
+                    LOG.error(message, ex);
+                    String details = "Please download the file\n\n"
+                            + "https://mgr.servers.aceworkspace.net/apps/va-archiva/repository/all/org/ihtsdo/otf/tcc-test-data/3.0/"
+                            + "\n\nand unzip it into\n\n"
+                            + System.getProperty("user.dir")
+                            + "\n\nand then restart the editor.";
+                    app.showErrorDialog(title, message, details);
+                } else {
+                    String title = "Unexpected error connecting to workbench database";
+                    String msg = ex.getClass().getName();
+                    String details = ex.getMessage();
+                    LOG.error(title, ex);
+                    app.showErrorDialog(title, msg, details);
+                }
+            }
+        };
 
-		Thread t = new Thread(task, "SCT_DB_Open");
-		t.setDaemon(true);
-		t.start();
-	}
+        Thread t = new Thread(task, "SCT_DB_Open");
+        t.setDaemon(true);
+        t.start();
+    }
 
-	protected BdbTerminologyStore getDataStore(String bdbFolderName) throws Exception {
+    protected BdbTerminologyStore getDataStore(String bdbFolderName) throws Exception {
 
-		// Default value if null.
-		if (bdbFolderName == null) {
-			bdbFolderName = "berkeley-db";  // Hard-coded in BdbTerminologyStore.
-		}
+        // Default value if null.
+        if (bdbFolderName == null) {
+            bdbFolderName = "berkeley-db";  // Hard-coded in BdbTerminologyStore.
+        }
 
-		// Sanity check.
-		File bdbFolderPath = new File(bdbFolderName);
-		if (! bdbFolderPath.exists()) {
-			throw new FileNotFoundException(bdbFolderName);
-		}
+        // Sanity check.
+        File bdbFolderPath = new File(bdbFolderName);
+        if (! bdbFolderPath.exists()) {
+            throw new FileNotFoundException(bdbFolderName);
+        }
 
-		return new BdbTerminologyStore();
-	}
+        return new BdbTerminologyStore();
+    }
 }
