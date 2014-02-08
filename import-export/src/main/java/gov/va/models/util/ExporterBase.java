60f9d3fcd5148067cd218b6ce94e4d7e6b953755
Owen Carlsen <ocarlsen@westcoastinformatics.com>
2014-02-07 16:11:59 -0800

    Creating separate ExporterBase, adding TODOs to factor out common logic.

--- import-export/src/main/java/gov/va/models/util/ExporterBase.java
+++ import-export/src/main/java/gov/va/models/util/ExporterBase.java
@@ -4,40 +4,32 @@ import gov.va.isaac.gui.AppContext;
 
 import java.io.IOException;
 
-import org.ihtsdo.otf.tcc.api.blueprint.TerminologyBuilderBI;
 import org.ihtsdo.otf.tcc.api.coordinate.EditCoordinate;
 import org.ihtsdo.otf.tcc.api.coordinate.StandardViewCoordinates;
 import org.ihtsdo.otf.tcc.api.coordinate.ViewCoordinate;
 import org.ihtsdo.otf.tcc.api.metadata.binding.Snomed;
 import org.ihtsdo.otf.tcc.api.metadata.binding.TermAux;
 import org.ihtsdo.otf.tcc.api.spec.ValidationException;
-import org.ihtsdo.otf.tcc.datastore.BdbTermBuilder;
 import org.ihtsdo.otf.tcc.datastore.BdbTerminologyStore;
 
 /**
- * Base class containing common methods for importing terminology objects.
+ * Base class containing common methods for exporting terminology objects.
  *
- * TODO: Factor out common logic with {@link ExporterBase}.
+ * TODO: Factor out common logic with {@link ImporterBase}.
  *
  * @author ocarlsen
  */
-public class ImporterBase {
+public class ExporterBase {
 
     private final BdbTerminologyStore dataStore;
-    private final TerminologyBuilderBI builder;
 
-    protected ImporterBase(AppContext appContext) throws ValidationException, IOException {
+    protected ExporterBase(AppContext appContext) throws ValidationException, IOException {
         super();
         this.dataStore = appContext.getDataStore();
-        this.builder = new BdbTermBuilder(getEC(), getVC());
     }
 
     protected final BdbTerminologyStore getDataStore() {
         return dataStore;
-    }
-
-    protected final TerminologyBuilderBI getBuilder() {
-        return builder;
     }
 
     protected final EditCoordinate getEC() throws ValidationException, IOException {
