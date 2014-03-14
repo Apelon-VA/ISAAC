7d7b9e0436dd5769a74b7ba27e553844b2887e57
Owen Carlsen <ocarlsen@westcoastinformatics.com>
2014-03-14 16:38:45 -0700

    Correctly checking if filteredCount > 1 before logging warning.

--- import-export/src/main/java/gov/va/isaac/models/util/ExporterBase.java
+++ import-export/src/main/java/gov/va/isaac/models/util/ExporterBase.java
@@ -1,15 +1,15 @@
 /**
  * Copyright Notice
- * 
+ *
  * This is a work of the U.S. Government and is not subject to copyright
  * protection in the United States. Foreign copyrights may apply.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -69,10 +69,11 @@ public abstract class ExporterBase extends CommonBase {
         // is using refsets and the way import-export is.
         // Log it for now, resolve later.  (See artf229216.)
 //        Preconditions.checkState(filteredCount <= 1,
-        getLogger().warn(
-                "Expected 0-1 annotations for refset nid " + refsetSpec.getNid() +
-                ", found " + filteredCount);
-
+        if (filteredCount > 1) {
+            getLogger().warn(
+                    "Expected 0-1 annotations for refset nid " + refsetSpec.getNid() +
+                    ", found " + filteredCount);
+        }
 
         // Return annotation, or null if none.
         if (filteredCount == 0) {
