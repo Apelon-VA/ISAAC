3860923e145db20ce58bb1536bb0bee528905ab2
Owen Carlsen <ocarlsen@westcoastinformatics.com>
2014-04-01 16:42:07 -0700

    Logging warning instead of throwing exception if no properties file found.

--- isaac-app-interfaces/src/main/java/gov/va/isaac/AppProperties.java
+++ isaac-app-interfaces/src/main/java/gov/va/isaac/AppProperties.java
@@ -1,8 +1,12 @@
 package gov.va.isaac;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Functionality to provide properties to the ISAAC application and its modules.
@@ -11,19 +15,33 @@ import java.util.Properties;
  */
 public class AppProperties {
 
+    private static final Logger LOG = LoggerFactory.getLogger(AppProperties.class);
+    private static final String PROPERTIES_FILE = "/app.properties";
+
     private final Properties properties;
 
-    public AppProperties() throws IOException {
+    public AppProperties() {
         super();
         this.properties = new Properties();
 
         // Load from file in classpath.
-        InputStream in = getClass().getResourceAsStream("/app.properties");
-        properties.load(in);
+        try {
+            InputStream in = getClass().getResourceAsStream(PROPERTIES_FILE);
+            if (in == null) {
+                throw new FileNotFoundException(PROPERTIES_FILE);
+            }
+            properties.load(in);
+        } catch (IOException ex) {
+            LOG.warn("Unable to load app properties.", ex);
+        }
     }
 
     public String getProperty(String key) {
         return properties.getProperty(key);
+    }
+
+    public void setProperty(String key, String value) {
+        properties.setProperty(key, value);
     }
 }
 
