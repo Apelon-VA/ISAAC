db1b50d5a20c88e2241b06f2a367ac6adc35b287
jefron-ap <apelonpw>
2014-09-22 20:39:28 +0300

    Better handling of changes... Display last change not last committed change in modeling popup and only underline modified attributes in Enhanced Concept View

--- otf-util/src/main/java/gov/va/isaac/gui/conceptViews/componentRows/DetailRelRow.java
+++ otf-util/src/main/java/gov/va/isaac/gui/conceptViews/componentRows/DetailRelRow.java
@@ -11,6 +11,7 @@ import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Priority;
 import javafx.scene.shape.Rectangle;
 
+import org.ihtsdo.otf.tcc.api.chronicle.ComponentChronicleBI;
 import org.ihtsdo.otf.tcc.api.relationship.RelationshipVersionBI;
 
 public class DetailRelRow extends RelRow {
@@ -34,12 +35,29 @@ public class DetailRelRow extends RelRow {
 		Label relRefLabel = labelHelper.createLabel(rel, WBUtility.getConPrefTerm(rel.getRefinabilityNid()), ComponentType.RELATIONSHIP, rel.getRefinabilityNid());
 		
 		if (rel.isUncommitted()) {
-			relLabel.setUnderline(true);
-			relTypeLabel.setUnderline(true);
-			relCharLabel.setUnderline(true);
-			relRefLabel.setUnderline(true);
-		}
+			ComponentChronicleBI<?> chronicle = rel.getChronicle();
+			RelationshipVersionBI<?> origVersion = (RelationshipVersionBI<?>) chronicle.getVersions().toArray()[chronicle.getVersions().size() - 2];
 
+			if (!relLabel.getText().equals(WBUtility.getConPrefTerm(origVersion.getDestinationNid()))) {
+				relLabel.setUnderline(true);
+			}
+			
+			if (!relTypeLabel.getText().equals(WBUtility.getConPrefTerm(origVersion.getTypeNid()))) {
+				relTypeLabel.setUnderline(true);
+			}
+
+			if (!relCharLabel.getText().equals(WBUtility.getConPrefTerm(origVersion.getCharacteristicNid()))) {
+				relCharLabel.setUnderline(true);
+			}
+			
+			if (!relRefLabel.getText().equals(WBUtility.getConPrefTerm(origVersion.getRefinabilityNid()))) {
+				relRefLabel.setUnderline(true);
+			}
+			
+			if (!relGroupLabel.getText().equals(origVersion.getGroup())) {
+				relGroupLabel.setUnderline(true);
+			}
+		}
 		//setConstraints(Node child, int columnIndex, int rowIndex, int columnspan, int rowspan, HPos halignment, 
 		//				 VPos valignment, Priority hgrow, Priority vgrow, Insets margin)
 		GridPane.setConstraints(rec,  0,  0,  1,  1,  HPos.CENTER,  VPos.CENTER, Priority.NEVER, Priority.ALWAYS);
