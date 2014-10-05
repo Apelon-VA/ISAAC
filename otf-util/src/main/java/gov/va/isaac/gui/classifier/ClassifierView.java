4366dbcd2306dd453a0585e45c72d4dc21050267
Tun Tun Naing <tnaing@westcoastinformatics.com>
2014-10-04 18:22:30 -0700

    artf230762 integrate existing EL++ to ISAAC

---
+++ otf-util/src/main/java/gov/va/isaac/gui/classifier/ClassifierView.java
@@ -0,0 +1,115 @@
+/**
+ * Copyright Notice
+ * 
+ * This is a work of the U.S. Government and is not subject to copyright
+ * protection in the United States. Foreign copyrights may apply.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package gov.va.isaac.gui.classifier;
+
+import gov.va.isaac.AppContext;
+import gov.va.isaac.interfaces.gui.ApplicationMenus;
+import gov.va.isaac.interfaces.gui.MenuItemI;
+import gov.va.isaac.interfaces.gui.views.IsaacViewWithMenusI;
+import gov.va.isaac.interfaces.gui.views.PopupViewI;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javafx.stage.Window;
+
+import javax.inject.Singleton;
+
+import org.jvnet.hk2.annotations.Service;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * CEMMetadataCreatorView
+ * 
+ * @author ocarlsen
+ * @author <a href="mailto:daniel.armbrust.list@gmail.com">Dan Armbrust</a>
+ */
+@Service
+@Singleton
+public class ClassifierView implements PopupViewI, IsaacViewWithMenusI
+{
+	private final Logger LOG = LoggerFactory.getLogger(this.getClass());
+
+	/**
+	 * @see gov.va.isaac.interfaces.gui.views.IsaacViewI#getMenuBarMenus()
+	 */
+	@Override
+	public List<MenuItemI> getMenuBarMenus()
+	{
+		ArrayList<MenuItemI> menus = new ArrayList<>();
+		menus.add(new MenuItemI()
+		{
+			@Override
+			public void handleMenuSelection(Window parent)
+			{
+				showView(parent);
+			}
+			
+			@Override
+			public int getSortOrder()
+			{
+				return 4;
+			}
+			
+			@Override
+			public String getParentMenuId()
+			{
+				return ApplicationMenus.ACTIONS.getMenuId();
+			}
+			
+			@Override
+			public String getMenuName()
+			{
+				return "Run Classifier";
+			}
+			
+			@Override
+			public String getMenuId()
+			{
+				return "runClassifierMenuItem";
+			}
+			
+			@Override
+			public boolean enableMnemonicParsing()
+			{
+				return false;
+			}
+		});
+		return menus;
+	}
+
+	/**
+	 * @see gov.va.isaac.interfaces.gui.views.PopupViewI#showView(javafx.stage.Window)
+	 */
+	@Override
+	public void showView(Window parent)
+	{
+		try {
+			ClassifierDialog dialog = new ClassifierDialog();
+			dialog.show();
+		} catch (IOException ex) {
+			String title = ex.getClass().getName();
+			String msg = String.format("Unexpected error showing ClassifierDialog");
+			LOG.error(msg, ex);
+			AppContext.getCommonDialogs().showErrorDialog(title, msg, ex.getMessage());
+		}
+	}
+}
