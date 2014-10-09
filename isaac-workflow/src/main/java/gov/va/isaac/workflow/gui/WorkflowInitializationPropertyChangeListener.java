5b4b0c2944cb55dae6e0f30bc46f972f773bf183
jefron-ap <apelonpw>
2014-10-09 15:18:19 -0500

    Add pre-commit identification of component to send to WF and post-commit add.  Added as long as not already in WF task claimed by user or already started by user.

--- isaac-workflow/src/main/java/gov/va/isaac/workflow/gui/WorkflowInitializationPropertyChangeListener.java
+++ isaac-workflow/src/main/java/gov/va/isaac/workflow/gui/WorkflowInitializationPropertyChangeListener.java
@@ -24,13 +24,28 @@ import gov.va.isaac.config.profiles.UserProfileManager;
 import gov.va.isaac.interfaces.gui.views.WorkflowInitiationViewI;
 import gov.va.isaac.interfaces.utility.DialogResponse;
 import gov.va.isaac.interfaces.utility.ServicesToPreloadI;
+import gov.va.isaac.interfaces.workflow.ProcessInstanceCreationRequestI;
+import gov.va.isaac.util.WBUtility;
+import gov.va.isaac.workflow.LocalTask;
+import gov.va.isaac.workflow.LocalTasksServiceBI;
+import gov.va.isaac.workflow.ProcessInstanceServiceBI;
+import gov.va.isaac.workflow.engine.RemoteWfEngine;
+import gov.va.isaac.workflow.exceptions.DatastoreException;
+
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
+import java.rmi.RemoteException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
 import javafx.application.Platform;
 import javax.inject.Singleton;
 import org.ihtsdo.otf.tcc.api.nid.NativeIdSetBI;
 import org.ihtsdo.otf.tcc.api.store.TerminologyDI.CONCEPT_EVENT;
+import org.ihtsdo.otf.tcc.model.cc.concept.ConceptChronicle;
 import org.jvnet.hk2.annotations.Service;
+import org.kie.api.task.TaskService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -49,6 +64,9 @@ public class WorkflowInitializationPropertyChangeListener implements PropertyCha
 {
 	private final Logger LOG = LoggerFactory.getLogger(this.getClass());
 	private boolean enabled = false;
+	private List<Integer> componentsToAdd;
+	private static LocalTasksServiceBI localTasksService = null;
+    private static ProcessInstanceServiceBI processService = null;
 
 	private WorkflowInitializationPropertyChangeListener()
 	{
@@ -76,7 +94,10 @@ public class WorkflowInitializationPropertyChangeListener implements PropertyCha
 		{
 			LOG.info("Enabling the workflow commit listener");
 			ExtendedAppContext.getDataStore().addPropertyChangeListener(CONCEPT_EVENT.POST_COMMIT, this);
+			ExtendedAppContext.getDataStore().addPropertyChangeListener(CONCEPT_EVENT.PRE_COMMIT, this);
 			enabled = true;
+			localTasksService = AppContext.getService(LocalTasksServiceBI.class);
+	        processService = AppContext.getService(ProcessInstanceServiceBI.class);
 		}
 	}
 
@@ -85,17 +106,21 @@ public class WorkflowInitializationPropertyChangeListener implements PropertyCha
 	{
 		try
 		{
-			if (CONCEPT_EVENT.POST_COMMIT.name().equals(evt.getPropertyName()))
+			if (CONCEPT_EVENT.PRE_COMMIT.name().equals(evt.getPropertyName()))
 			{
-				LOG.debug("post-commit triggered in workflow listener");
+				LOG.debug("pre-commit triggered in workflow listener");
 				int[] allConceptNids = ((NativeIdSetBI) evt.getNewValue()).getSetValues();
 
+				componentsToAdd = identifyComponents(allConceptNids);
+			} else if (CONCEPT_EVENT.POST_COMMIT.name().equals(evt.getPropertyName())) {
+				LOG.debug("post-commit triggered in workflow listener");
+
 				Platform.runLater(() -> {
-					boolean showEm = allConceptNids.length <= 20;
+					boolean showEm = componentsToAdd.size() <= 20;
 					
 					if (!showEm)
 					{
-						DialogResponse answer = AppContext.getCommonDialogs().showYesNoDialog("Show Workflow Dialog for each concept?", allConceptNids.length 
+						DialogResponse answer = AppContext.getCommonDialogs().showYesNoDialog("Show Workflow Dialog for each concept?", componentsToAdd.size() 
 								+ " concepts were just committed to the DB.  Really show a workflow dialog for each concept?");
 						if (answer == DialogResponse.YES)
 						{
@@ -104,13 +129,15 @@ public class WorkflowInitializationPropertyChangeListener implements PropertyCha
 					}
 					if (showEm)
 					{
-						for (int nid : allConceptNids)
+						for (int nid : componentsToAdd)
 						{
 							WorkflowInitiationViewI view = AppContext.getService(WorkflowInitiationViewI.class);
 							view.setComponent(nid);
 							view.showView(AppContext.getMainApplicationWindow().getPrimaryStage());
 						}
 					}
+					
+					componentsToAdd.clear();
 				});
 			}
 		}
@@ -120,6 +147,38 @@ public class WorkflowInitializationPropertyChangeListener implements PropertyCha
 		}
 	}
 
+	private List<Integer> identifyComponents(int[] allConceptNids) {
+		List<Integer> componentsToWf = new ArrayList<Integer>();
+		
+		for (int i = 0; i < allConceptNids.length; i++) {
+			ConceptChronicle c = (ConceptChronicle) WBUtility.getConceptVersion(allConceptNids[i]).getChronicle();
+
+			int componentNid = 0;
+			for (int nid : c.getUncommittedNids().getListArray()) {					
+				if (componentNid == 0) {
+					componentNid = nid;
+				} else {
+					componentNid = allConceptNids[i];
+					break;
+				}
+			}
+			
+			try {
+				UUID componentId = WBUtility.getComponentChronicle(componentNid).getPrimordialUuid();
+				List<LocalTask> openTaskList = localTasksService.getOpenOwnedTasksByComponentId(componentId);
+				List<ProcessInstanceCreationRequestI> reqList = processService.getRequestsByComponentId(componentId);
+				
+				if (reqList.isEmpty() && openTaskList.isEmpty() && componentNid != 0) {
+					componentsToWf.add(componentNid);
+				}
+			} catch (DatastoreException e) {
+				e.printStackTrace();
+			}
+		}
+
+		return componentsToWf;
+	}
+	
 	/**
 	 * @see gov.va.isaac.interfaces.utility.ServicesToPreloadI#loadRequested()
 	 */
