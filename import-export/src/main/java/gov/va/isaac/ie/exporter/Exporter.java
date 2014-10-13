9545c90c4e73ead02357d409b38d6ddfd6970176
bcarlsenca <bcarlsen@westcoastinformatics.com>
2014-10-13 11:29:58 -0700

    add missing class

---
+++ import-export/src/main/java/gov/va/isaac/ie/exporter/Exporter.java
@@ -0,0 +1,19 @@
+package gov.va.isaac.ie.exporter;
+
+import gov.va.isaac.util.ProgressReporter;
+
+
+/**
+ * Generically represents an Exporter.
+ */
+public interface Exporter extends ProgressReporter {
+
+  /**
+   * Exports the nid.
+   *
+   * @param pathNid the path nid
+   * @throws Exception 
+   */
+  public void export(int pathNid) throws Exception;
+  
+}
