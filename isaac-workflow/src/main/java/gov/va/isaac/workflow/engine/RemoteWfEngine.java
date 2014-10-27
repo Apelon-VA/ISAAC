4f820373b2e797c7c19c3282247ca6721fefaa58
darmbrust <daniel.armbrust.list@gmail.com>
2014-10-27 02:42:39 -0500

    prompt for the WF credentials, if the stored credentials don't work.

--- isaac-workflow/src/main/java/gov/va/isaac/workflow/engine/RemoteWfEngine.java
+++ isaac-workflow/src/main/java/gov/va/isaac/workflow/engine/RemoteWfEngine.java
@@ -20,12 +20,17 @@ package gov.va.isaac.workflow.engine;
 
 import gov.va.isaac.AppContext;
 import gov.va.isaac.ExtendedAppContext;
-
+import gov.va.isaac.config.profiles.UserProfile;
+import gov.va.isaac.config.profiles.UserProfileManager;
+import gov.va.isaac.gui.users.Credentials;
+import gov.va.isaac.gui.users.CredentialsPromptDialog;
+import gov.va.isaac.workflow.persistence.LocalTasksApi;
 import java.net.URL;
 import java.rmi.RemoteException;
-
+import java.util.concurrent.CountDownLatch;
+import java.util.function.Consumer;
+import javafx.application.Platform;
 import javax.inject.Singleton;
-
 import org.jvnet.hk2.annotations.Service;
 import org.kie.api.runtime.manager.RuntimeEngine;
 import org.kie.api.task.TaskService;
@@ -46,6 +51,9 @@ public class RemoteWfEngine
 	private static final Logger LOG = LoggerFactory.getLogger(RemoteWfEngine.class);
 
 	private RuntimeEngine remoteEngine;
+	
+	private transient Credentials promptResult;
+	private CountDownLatch waitForPrompt = null;
 
 	private RemoteWfEngine()
 	{
@@ -61,24 +69,77 @@ public class RemoteWfEngine
 		}
 		else
 		{
-			//TODO DAN prompt the user, block, wait for their response - let them update credentials
+			final String deploymentId = AppContext.getAppConfiguration().getWorkflowServerDeploymentId();
+			final URL workflowServerUrlAsURL = AppContext.getAppConfiguration().getWorkflowServerUrlAsURL();
+			final String workflowUserName = ExtendedAppContext.getCurrentlyLoggedInUserProfile().getWorkflowUsername();
+			final String workflowPassword = ExtendedAppContext.getCurrentlyLoggedInUserProfile().getWorkflowPassword();
 			try
 			{
-				final String deploymentId = AppContext.getAppConfiguration().getWorkflowServerDeploymentId();
-				final URL workflowServerUrlAsURL = AppContext.getAppConfiguration().getWorkflowServerUrlAsURL();
-				final String workflowUserName = ExtendedAppContext.getCurrentlyLoggedInUserProfile().getWorkflowUsername();
 				LOG.debug("Getting remote WF engine with deploymentId={}, workflowServerUrlAsURL={} and workflowUserName={}", deploymentId, workflowServerUrlAsURL, workflowUserName);
 				RemoteRestRuntimeFactory restSessionFactory = new RemoteRestRuntimeFactory(
 						deploymentId, 
 						workflowServerUrlAsURL, 
 						workflowUserName,
-						ExtendedAppContext.getCurrentlyLoggedInUserProfile().getWorkflowPassword());
+						workflowPassword);
 				re = restSessionFactory.newRuntimeEngine();
+				re.getTaskService().getTaskById(-1);  //Something to validate the credentials are correct...
 				remoteEngine = re;
 				return re;
 			}
 			catch (RuntimeException e)
 			{
+				if (Platform.isFxApplicationThread())
+				{
+					throw new RuntimeException("API Misuse!  Do not call a  remote operation on the Platform thread!");
+				}
+				
+				waitForPrompt = new CountDownLatch(1);
+				promptResult = null;
+				
+				Platform.runLater(() ->
+				{
+					AppContext.getService(CredentialsPromptDialog.class).showView(workflowUserName, workflowPassword, 
+							"Please provide your Workflow server (KIE) credentials", new Consumer<Credentials>()
+							{
+								@Override
+								public void accept(Credentials t)
+								{
+									promptResult = t;
+									waitForPrompt.countDown();
+								}
+							});
+				});
+				
+				try
+				{
+					waitForPrompt.await();
+					if (promptResult != null)
+					{
+						UserProfile up = AppContext.getService(UserProfileManager.class).getCurrentlyLoggedInUserProfile();
+						if (!up.getWorkflowUsername().equals(promptResult.getUsername()))
+						{
+							AppContext.getService(LocalTasksApi.class).changeUserName(up.getWorkflowUsername(), promptResult.getUsername());
+						}
+						up.setWorkflowUsername(promptResult.getUsername());
+						up.setWorkflowPassword(promptResult.getPassword());
+						try
+						{
+							AppContext.getService(UserProfileManager.class).saveChanges(up);
+						}
+						catch (Exception e1)
+						{
+							throw new RuntimeException("Unexpected error saving credentials");
+						}
+						return getRemoteEngine();  //loop
+					}
+					//else - just let the remote exception go back
+				}
+				catch (InterruptedException e1)
+				{
+					LOG.info("Interrupted while waiting for credentials");
+					// noop - just let the remote exception go back.
+				}
+				
 				throw new RemoteException("Error connecting to server", e);
 			}
 		}
