e0321add42ceeed901a18236c3e547fe153ed7e0
darmbrust <daniel.armbrust.list@gmail.com>
2014-11-09 23:10:21 -0600

    clean imports

--- lego-view/src/main/java/gov/va/legoEdit/gui/ExportDialog.java
+++ lego-view/src/main/java/gov/va/legoEdit/gui/ExportDialog.java
@@ -21,7 +21,6 @@ package gov.va.legoEdit.gui;
 import gov.va.legoEdit.model.schemaModel.LegoList;
 import java.io.IOException;
 import java.util.List;
-import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
