cb15e3c61b9dd3b219a46dfb43a2c5f08d0d1340
mygithubid <joel.kniaz@gmail.com>
2014-11-21 09:47:49 -0500

    Adding HeaderNode class to activate DynamicRefexView column headers

---
+++ refex-view/src/main/java/gov/va/isaac/gui/refexViews/refexEdit/HeaderNode.java
@@ -0,0 +1,98 @@
+/**
+ * Copyright Notice
+ * 
+ * This is a work of the U.S. Government and is not subject to copyright
+ * protection in the United States. Foreign copyrights may apply.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * HeaderNode
+ * 
+ * @author <a href="mailto:joel.kniaz@gmail.com">Joel Kniaz</a>
+ */
+package gov.va.isaac.gui.refexViews.refexEdit;
+
+import gov.va.isaac.gui.util.Images;
+
+import java.util.ArrayList;
+
+import javafx.collections.ListChangeListener;
+import javafx.collections.ObservableList;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TreeTableColumn;
+import javafx.scene.layout.HBox;
+import javafx.scene.paint.Color;
+
+import com.sun.javafx.collections.ObservableListWrapper;
+
+/**
+ * HeaderNode
+ * 
+ * @author <a href="mailto:joel.kniaz@gmail.com">Joel Kniaz</a>
+ *
+ *
+ */
+public class HeaderNode {
+	public interface Filter {
+		// Mockup
+	}
+	final ObservableList<Filter> filters = new ObservableListWrapper<Filter>(new ArrayList<>());
+	final HBox hbox = new HBox();
+	final Label label;
+	final Button filterConfigurationButton = new Button();
+	final TreeTableColumn<RefexDynamicGUI, ?> column;
+
+	public HeaderNode(TreeTableColumn<RefexDynamicGUI, ?> col) {
+		this(col, new Label(col.getText()));
+	}
+
+	public HeaderNode(TreeTableColumn<RefexDynamicGUI, ?> col, Label label) {
+		column = col;
+		this.label = label;
+		filterConfigurationButton.setGraphic(Images.EXCLAMATION.createImageView());
+		
+		hbox.getChildren().addAll(label, filterConfigurationButton);
+
+		filters.addListener(new ListChangeListener<Filter>() {
+			@Override
+			public void onChanged(
+					javafx.collections.ListChangeListener.Change<? extends Filter> c) {
+				updateTextFillColor();
+			}
+		});
+		
+		filterConfigurationButton.setOnAction(event -> { System.out.println("Clicked filterConfigurationButton"); });
+
+		updateTextFillColor();
+	}
+	
+	public ObservableList<Filter> getFilters() { return filters; }
+	public Label getLabel() { return label; }
+	public Button getButton() { return filterConfigurationButton; }
+	public TreeTableColumn<RefexDynamicGUI, ?> getColumn() { return column; }
+	
+	public javafx.scene.Node getNode() { return hbox; }
+	
+	private void updateTextFillColor() {
+		Color color = Color.BLACK;
+		if (filters.size() == 0) {
+			color = Color.BLACK;
+		} else {
+			color = Color.RED;
+		}
+		label.setTextFill(color);
+	}
+}
