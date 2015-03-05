479da798a5082992724ee1acc96cfe5fa4f6066f
vkaloidis <vkaloidis@apelon.com>
2015-03-05 09:40:15 -0500

    Initial development for that artifact [artf233797]

---
+++ mapping/src/main/java/gov/va/isaac/gui/mapping/CreateMappingSetController.java
@@ -0,0 +1,149 @@
+package gov.va.isaac.gui.mapping;
+
+import gov.va.isaac.gui.util.ErrorMarkerUtils;
+import gov.va.isaac.util.TaskCompleteCallback;
+import gov.va.isaac.util.ValidBooleanBinding;
+
+import java.io.IOException;
+import java.net.URL;
+
+import javafx.beans.property.StringProperty;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
+import javafx.scene.layout.Region;
+import javafx.scene.layout.StackPane;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Controller class for the Create Mapping View.
+ *
+ * @author vkaloidis
+ * 
+ */
+
+public class CreateMappingSetController implements TaskCompleteCallback {
+	private static final Logger logger = LoggerFactory.getLogger(MappingController.class);
+	
+	private Region region = new Region();
+	private Label title = new Label();
+	
+	@FXML private BorderPane	mainPane;
+	@FXML private TextField		nameInput;
+	@FXML private TextArea		descInput;
+	@FXML private TextField		purposeInput;
+	@FXML private Button		createButton;
+	@FXML private Button		cancelButton;
+	
+	
+	public Region getRootNode() {
+		return region;
+	}
+	
+	public StringProperty getTitle() {
+		return title.textProperty();
+	}
+	
+	@FXML
+	public void initialize() {
+		assert mainPane 			!= null : "fx:id=\"mainPane\" was not injected: check your FXML file 'CreateMapping.fxml'.";
+		assert nameInput 			!= null : "fx:id=\"nameInput\" was not injected: check your FXML file 'CreateMapping.fxml'.";
+		assert descInput 			!= null : "fx:id=\"descInput\" was not injected: check your FXML file 'CreateMapping.fxml'.";
+		assert purposeInput 		!= null : "fx:id=\"purposeInput\" was not injected: check your FXML file 'CreateMapping.fxml'.";
+		assert createButton 		!= null : "fx:id=\"createButton\" was not injected: check your FXML file 'CreateMapping.fxml'.";
+		assert cancelButton 		!= null : "fx:id=\"cancelButton\" was not injected: check your FXML file 'CreateMapping.fxml'.";
+		
+
+//		nameInput.setPromptText("CURRENT NAME"); TODO: vk EDIT MAPPING FILLS THIS OUT
+//		descInput.setPromptText("CURRENT DESC"); TODO: VK EDIT MAPPING FILLS THS OUT
+//		promptInput.setPromptText("CURRENT PROMPT") TODO: VK EDIT MAPPING FILLS THIS OUT 
+		
+		StackPane nameStack = new StackPane();
+		StackPane descStack = new StackPane();
+		title.setText("Create new Mapping Refset");;
+		
+		final ValidBooleanBinding nameInputEmpty = new ValidBooleanBinding() {
+			{
+				bind(nameInput.textProperty());
+				setComputeOnInvalidate(true);
+			}
+			@Override
+			protected boolean computeValue() {
+				if (nameInput.getText().length() == 0) {
+					setInvalidReason("You must fill out the Name Input and Description Input");
+					return false;
+				}
+				clearInvalidReason();
+				return true;
+			}
+		};
+		
+		final ValidBooleanBinding descInputEmpty = new ValidBooleanBinding() {
+			{
+				bind(descInput.textProperty());
+				setComputeOnInvalidate(true);
+			}
+			@Override
+			protected boolean computeValue() {
+				if (descInput.getText().length() == 0) {
+					setInvalidReason("You must fill out the Name Input and Description Input");
+					return false;
+				}
+				clearInvalidReason();
+				return true;
+			}
+		};
+		ErrorMarkerUtils.setupErrorMarker(nameInput, nameStack, nameInputEmpty);
+		ErrorMarkerUtils.setupErrorMarker(descInput, descStack, descInputEmpty);
+		
+		createButton.setDefaultButton(true);
+		createButton.setPadding(new Insets(5, 20, 5, 20));
+		createButton.disableProperty().bind(nameInputEmpty.not());
+		createButton.disableProperty().bind(descInputEmpty.not());
+		createButton.setOnAction((event) -> {
+			// TODO: vk CREATE LOGIC TO PASS ON TO NEXT STEP
+		});
+		
+		
+		HBox.setHgrow(region, Priority.ALWAYS); //TODO: vk maybe get rid of this bc its done in fxml file
+		cancelButton.setPadding(new Insets(5, 20, 5, 20));
+		cancelButton.setCancelButton(true);
+		cancelButton.setOnKeyPressed(new EventHandler<KeyEvent>()  {
+			@Override
+			public void handle(KeyEvent event) {
+				if (event.getCode() == KeyCode.ENTER) {
+					event.consume();
+					cancelButton.fire();
+				}
+			}
+		});
+		
+		HBox buttons = new HBox();
+		buttons.setMaxWidth(Double.MAX_VALUE);
+		buttons.setPadding(new Insets(10));
+		buttons.setSpacing(5);
+		buttons.getChildren().add(cancelButton);
+		buttons.getChildren().add(createButton);
+		
+	}
+
+	@Override
+    public void taskComplete(long taskStartTime, Integer taskId) {
+	    // TODO Auto-generated method stub
+	    
+    }
+}
+
+
--- mapping/src/main/java/gov/va/isaac/gui/mapping/MappingController.java
+++ mapping/src/main/java/gov/va/isaac/gui/mapping/MappingController.java
@@ -1,102 +1,26 @@
 package gov.va.isaac.gui.mapping;
 
 import gov.va.isaac.AppContext;
-import gov.va.isaac.ExtendedAppContext;
-import gov.va.isaac.gui.ConceptNode;
-import gov.va.isaac.gui.ConfigureDynamicRefexIndexingView;
-//import gov.va.isaac.gui.IndexStatusListener;
-import gov.va.isaac.gui.SimpleDisplayConcept;
-import gov.va.isaac.gui.dragAndDrop.DragRegistry;
-import gov.va.isaac.gui.dragAndDrop.SingleConceptIdProvider;
 import gov.va.isaac.gui.util.FxUtils;
 import gov.va.isaac.gui.util.Images;
-import gov.va.isaac.search.CompositeSearchResult;
-import gov.va.isaac.search.SearchHandle;
-import gov.va.isaac.search.SearchHandler;
-import gov.va.isaac.util.CommonMenuBuilderI;
-import gov.va.isaac.util.CommonMenus;
-import gov.va.isaac.util.CommonMenusDataProvider;
-import gov.va.isaac.util.CommonMenusNIdProvider;
-import gov.va.isaac.util.Interval;
-import gov.va.isaac.util.NumberUtilities;
 import gov.va.isaac.util.TaskCompleteCallback;
-import gov.va.isaac.util.Utility;
-import gov.va.isaac.util.ValidBooleanBinding;
-import gov.va.isaac.util.OTFUtility;
 
 import java.io.IOException;
 import java.net.URL;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.HashSet;
-import java.util.List;
-import java.util.ResourceBundle;
-import java.util.Set;
-import java.util.function.Consumer;
 
 import javafx.application.Platform;
-import javafx.beans.InvalidationListener;
-import javafx.beans.Observable;
-import javafx.beans.property.BooleanProperty;
-import javafx.beans.property.SimpleBooleanProperty;
-import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
-import javafx.collections.ObservableList;
-import javafx.concurrent.Task;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.geometry.Insets;
-import javafx.scene.image.ImageView;
 import javafx.scene.control.Button;
-import javafx.scene.control.ButtonBase;
-import javafx.scene.control.CheckBox;
-import javafx.scene.control.ChoiceBox;
-import javafx.scene.control.ContextMenu;
 import javafx.scene.control.Label;
-import javafx.scene.control.ListCell;
-import javafx.scene.control.ListView;
-import javafx.scene.control.MenuItem;
-import javafx.scene.control.ProgressIndicator;
-import javafx.scene.control.SelectionMode;
 import javafx.scene.control.TableView;
-import javafx.scene.control.TextField;
-import javafx.scene.control.TitledPane;
 import javafx.scene.control.ToggleButton;
-import javafx.scene.control.ToolBar;
-import javafx.scene.control.Tooltip;
-import javafx.scene.input.MouseButton;
-import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.FlowPane;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.Priority;
-import javafx.scene.layout.StackPane;
-import javafx.scene.layout.VBox;
-import javafx.util.Callback;
-import javafx.util.StringConverter;
 
-import org.ihtsdo.otf.query.lucene.LuceneDynamicRefexIndexer;
-import org.ihtsdo.otf.query.lucene.LuceneDynamicRefexIndexerConfiguration;
-import org.ihtsdo.otf.tcc.api.chronicle.ComponentVersionBI;
-import org.ihtsdo.otf.tcc.api.concept.ConceptVersionBI;
-import org.ihtsdo.otf.tcc.api.metadata.binding.RefexDynamic;
-import org.ihtsdo.otf.tcc.api.refexDynamic.RefexDynamicChronicleBI;
-import org.ihtsdo.otf.tcc.api.refexDynamic.RefexDynamicVersionBI;
-import org.ihtsdo.otf.tcc.api.refexDynamic.data.RefexDynamicColumnInfo;
-import org.ihtsdo.otf.tcc.api.refexDynamic.data.RefexDynamicDataBI;
-import org.ihtsdo.otf.tcc.api.refexDynamic.data.RefexDynamicDataType;
-import org.ihtsdo.otf.tcc.api.refexDynamic.data.RefexDynamicUsageDescription;
-import org.ihtsdo.otf.tcc.model.cc.refexDynamic.data.dataTypes.RefexDynamicString;
-import org.ihtsdo.otf.tcc.model.index.service.IndexerBI;
-import org.ihtsdo.otf.tcc.model.index.service.SearchResult;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import com.sun.javafx.collections.ObservableListWrapper;
 
 
 /**
@@ -167,6 +91,17 @@ public class MappingController implements TaskCompleteCallback
 		FxUtils.assignImageToButton(commentButton, 		Images.BALLOON.createImageView(), 	"View Comments");
 		
 		activeOnlyToggle.setSelected(true);
+		
+		plusMappingButton.setOnAction(new EventHandler<ActionEvent>()
+		{
+			@Override
+			public void handle(ActionEvent e) {
+				CreateMappingSetView cv = AppContext.getService(CreateMappingSetView.class);
+				cv.setMapping("New Name", "New Desc", "New Purpose");
+				cv.showView(null);
+			}
+		});
+		
 	}
 
 	public AnchorPane getRoot()
