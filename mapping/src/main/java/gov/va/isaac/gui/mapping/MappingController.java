8633039f7714e1d83ad36498514b7e80437de169
dtriglianos <dtriglianos@apelon.com>
2015-03-20 13:30:13 -0400

    Intermediate commit

--- mapping/src/main/java/gov/va/isaac/gui/mapping/MappingController.java
+++ mapping/src/main/java/gov/va/isaac/gui/mapping/MappingController.java
@@ -6,10 +6,13 @@ import gov.va.isaac.gui.mapping.data.MappingSet;
 import gov.va.isaac.gui.util.FxUtils;
 import gov.va.isaac.gui.util.Images;
 import gov.va.isaac.util.OTFUtility;
+
 import java.io.IOException;
 import java.net.URL;
 import java.util.UUID;
+
 import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ListChangeListener;
@@ -23,9 +26,12 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableColumn.CellDataFeatures;
 import javafx.scene.control.TableView;
+import javafx.scene.control.Toggle;
 import javafx.scene.control.ToggleButton;
+import javafx.scene.control.ToggleGroup;
 import javafx.scene.layout.AnchorPane;
 import javafx.util.Callback;
+
 import org.ihtsdo.otf.tcc.api.contradiction.ContradictionException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -45,17 +51,17 @@ public class MappingController {
     @FXML private AnchorPane	mappingPane;
     @FXML private AnchorPane	listPane;
     @FXML private ToggleButton 	activeOnlyToggle;
-    @FXML private Button 		plusMappingButton;
-    @FXML private Button 		minusMappingButton;
-    @FXML private Button 		editMappingButton;
-	@FXML private Label			mappingSummaryLabel;
-	@FXML private TableView<MappingSet> mappingTableView;
-	@FXML private Label			listTitleLabel;
-	@FXML private TableView<MappingItem> listTableView;
-    @FXML private Button 		plusListButton;
-    @FXML private Button 		minusListButton;
+    @FXML private Button 		plusMappingSetButton;
+    @FXML private Button 		minusMappingSetButton;
+    @FXML private Button 		editMappingSetButton;
+	@FXML private TableView<MappingSet> mappingSetTableView;
+	@FXML private Label			mappingItemListTitleLabel;
+	@FXML private TableView<MappingItem> mappingItemTableView;
+    @FXML private Button 		plusMappingItemButton;
+    @FXML private Button 		minusMappingItemButton;
     @FXML private Button 		commentButton;
-    @FXML private Label			listSummaryLabel;
+	@FXML private Label			mappingSetSummaryLabel;
+    @FXML private Label			mappingItemSummaryLabel;
     
 	public static MappingController init() throws IOException {
 		// Load from FXML.
@@ -72,33 +78,42 @@ public class MappingController {
 		assert mappingPane 			!= null : "fx:id=\"mappingPane\" was not injected: check your FXML file 'Mapping.fxml'.";
 		assert listPane 			!= null : "fx:id=\"listPane\" was not injected: check your FXML file 'Mapping.fxml'.";
 		assert activeOnlyToggle 	!= null : "fx:id=\"activeOnlyToggle\" was not injected: check your FXML file 'Mapping.fxml'.";
-		assert plusMappingButton 	!= null : "fx:id=\"plusMappingButton\" was not injected: check your FXML file 'Mapping.fxml'.";
-		assert minusMappingButton	!= null : "fx:id=\"minusMappingButton\" was not injected: check your FXML file 'Mapping.fxml'.";
-		assert editMappingButton 	!= null : "fx:id=\"editMappingButton\" was not injected: check your FXML file 'Mapping.fxml'.";
+		assert plusMappingSetButton 	!= null : "fx:id=\"plusMappingButton\" was not injected: check your FXML file 'Mapping.fxml'.";
+		assert minusMappingSetButton	!= null : "fx:id=\"minusMappingButton\" was not injected: check your FXML file 'Mapping.fxml'.";
+		assert editMappingSetButton 	!= null : "fx:id=\"editMappingButton\" was not injected: check your FXML file 'Mapping.fxml'.";
 
-		assert mappingSummaryLabel 	!= null : "fx:id=\"mappingSummaryLabel\" was not injected: check your FXML file 'Mapping.fxml'.";
-		assert mappingTableView 	!= null : "fx:id=\"mappingTableView\" was not injected: check your FXML file 'Mapping.fxml'.";
-		assert listTitleLabel 		!= null : "fx:id=\"listTitleLabel\" was not injected: check your FXML file 'Mapping.fxml'.";
-		assert listTableView 		!= null : "fx:id=\"listTableView\" was not injected: check your FXML file 'Mapping.fxml'.";
-		assert plusListButton 		!= null : "fx:id=\"plusListButton\" was not injected: check your FXML file 'Mapping.fxml'.";
-		assert minusListButton 		!= null : "fx:id=\"minusListButton\" was not injected: check your FXML file 'Mapping.fxml'.";
+		assert mappingSetSummaryLabel 	!= null : "fx:id=\"mappingSummaryLabel\" was not injected: check your FXML file 'Mapping.fxml'.";
+		assert mappingSetTableView 	!= null : "fx:id=\"mappingTableView\" was not injected: check your FXML file 'Mapping.fxml'.";
+		assert mappingItemListTitleLabel 		!= null : "fx:id=\"listTitleLabel\" was not injected: check your FXML file 'Mapping.fxml'.";
+		assert mappingItemTableView != null : "fx:id=\"listTableView\" was not injected: check your FXML file 'Mapping.fxml'.";
+		assert plusMappingItemButton 		!= null : "fx:id=\"plusListButton\" was not injected: check your FXML file 'Mapping.fxml'.";
+		assert minusMappingItemButton 		!= null : "fx:id=\"minusListButton\" was not injected: check your FXML file 'Mapping.fxml'.";
 		assert commentButton 		!= null : "fx:id=\"commentButton\" was not injected: check your FXML file 'Mapping.fxml'.";
-		assert listSummaryLabel 	!= null : "fx:id=\"listSummaryLabel\" was not injected: check your FXML file 'Mapping.fxml'.";
+		assert mappingItemSummaryLabel 	!= null : "fx:id=\"listSummaryLabel\" was not injected: check your FXML file 'Mapping.fxml'.";
 
 		
 		mainPane.getStylesheets().add(MappingController.class.getResource("/isaac-shared-styles.css").toString());
 		
 		FxUtils.assignImageToButton(activeOnlyToggle, 	Images.FILTER_16.createImageView(), "Show Active Only / Show All");
-		FxUtils.assignImageToButton(plusMappingButton, 	Images.PLUS.createImageView(), 		"Create Mapping Set");
-		FxUtils.assignImageToButton(minusMappingButton, Images.MINUS.createImageView(), 	"Retire Mapping Set");
-		FxUtils.assignImageToButton(editMappingButton, 	Images.EDIT.createImageView(), 		"Edit Mapping Set");
-		FxUtils.assignImageToButton(plusListButton, 	Images.PLUS.createImageView(), 		"Create Mapping");
-		FxUtils.assignImageToButton(minusListButton, 	Images.MINUS.createImageView(), 	"Retire Mapping");
+		FxUtils.assignImageToButton(plusMappingSetButton, 	Images.PLUS.createImageView(), 		"Create Mapping Set");
+		FxUtils.assignImageToButton(minusMappingSetButton, Images.MINUS.createImageView(), 	"Retire Mapping Set");
+		FxUtils.assignImageToButton(editMappingSetButton, 	Images.EDIT.createImageView(), 		"Edit Mapping Set");
+		FxUtils.assignImageToButton(plusMappingItemButton, 	Images.PLUS.createImageView(), 		"Create Mapping");
+		FxUtils.assignImageToButton(minusMappingItemButton, 	Images.MINUS.createImageView(), 	"Retire Mapping");
 		FxUtils.assignImageToButton(commentButton, 		Images.BALLOON.createImageView(), 	"View Comments");
 		
+		ToggleGroup activeOnlyToggleGroup = new ToggleGroup();
+		activeOnlyToggle.setToggleGroup(activeOnlyToggleGroup);
 		activeOnlyToggle.setSelected(true);
 		
-		plusMappingButton.setOnAction(new EventHandler<ActionEvent>() {
+		activeOnlyToggleGroup.selectedToggleProperty().addListener(new ChangeListener<Toggle>() {
+			@Override
+			public void changed(ObservableValue<? extends Toggle> ov, Toggle toggle, Toggle new_toggle) {
+	        	readData();
+			}
+		});
+		
+		plusMappingSetButton.setOnAction(new EventHandler<ActionEvent>() {
 			@Override
 			public void handle(ActionEvent e) {
 				CreateMappingSetView cv = AppContext.getService(CreateMappingSetView.class);
@@ -107,7 +122,7 @@ public class MappingController {
 			}
 		});
 		
-		plusListButton.setOnAction(new EventHandler<ActionEvent>() {
+		plusMappingItemButton.setOnAction(new EventHandler<ActionEvent>() {
 			@Override
 			public void handle(ActionEvent e) {
 				CreateMappingItemView itemView = AppContext.getService(CreateMappingItemView.class);
@@ -125,7 +140,7 @@ public class MappingController {
 			}
 		});
 		
-		for (TableColumn<MappingSet, ?> x : mappingTableView.getColumns())
+		for (TableColumn<MappingSet, ?> x : mappingSetTableView.getColumns())
 		{
 			((TableColumn<MappingSet, String>)x).setCellValueFactory(new Callback<TableColumn.CellDataFeatures<MappingSet,String>, ObservableValue<String>>()
 			{
@@ -142,7 +157,12 @@ public class MappingController {
 					}
 					else if (param.getTableColumn().getText().equals("Status"))
 					{
-						return new SimpleStringProperty(param.getValue().getEditorStatus().toString().trim());
+						UUID editorStatus = param.getValue().getEditorStatus();
+						if (editorStatus == null) {
+							return new SimpleStringProperty("");
+						} else {
+							return new SimpleStringProperty(editorStatus.toString().trim());
+						}
 					}
 					else if (param.getTableColumn().getText().equals("Description"))
 					{
@@ -158,7 +178,7 @@ public class MappingController {
 			});
 		}
 		
-		mappingTableView.getSelectionModel().getSelectedItems().addListener(new ListChangeListener<MappingSet>()
+		mappingSetTableView.getSelectionModel().getSelectedItems().addListener(new ListChangeListener<MappingSet>()
 		{
 			@Override
 			public void onChanged(javafx.collections.ListChangeListener.Change<? extends MappingSet> c)
@@ -170,7 +190,7 @@ public class MappingController {
 			}
 		});
 		
-		for (TableColumn<MappingItem, ?> x : listTableView.getColumns())
+		for (TableColumn<MappingItem, ?> x : mappingItemTableView.getColumns())
 		{
 			((TableColumn<MappingItem, String>)x).setCellValueFactory(new Callback<TableColumn.CellDataFeatures<MappingItem,String>, ObservableValue<String>>()
 			{
@@ -198,8 +218,15 @@ public class MappingController {
 				}
 			});
 		}
+		
+		mappingSetSummaryLabel.setText("");
+		mappingItemSummaryLabel.setText("");
+		mappingItemListTitleLabel.setText("(no Mapping Set selected)");
+		mappingSetTableView.setPlaceholder(new Label("There are no Mapping Sets in the database."));
+		mappingItemTableView.setPlaceholder(new Label("No Mapping Set is selected."));
 	}
 		
+	
 	private void updateItemsList(MappingSet mappingSet)
 	{
 		ObservableList<MappingItem> mappingItems;
@@ -214,7 +241,13 @@ public class MappingController {
 			//TODO GUI prompt;
 			mappingItems = FXCollections.observableArrayList();
 		}
-		listTableView.setItems(mappingItems);
+		mappingItemTableView.setItems(mappingItems);
+
+		mappingItemListTitleLabel.setText(mappingSet.getName());
+		plusMappingItemButton.setDisable(false);
+		minusMappingSetButton.setDisable(false);
+		editMappingSetButton.setDisable(false);
+		mappingSetSummaryLabel.setText("Summary here.");
 	}
 
 	public AnchorPane getRoot()	{
@@ -224,8 +257,7 @@ public class MappingController {
 	protected void readData()
 	{
 		ObservableList<MappingSet> mappingSets;
-		boolean activeOnly = false;
-		//TODO retrieve active only flag
+		boolean activeOnly = activeOnlyToggle.isSelected();
 		try
 		{
 			mappingSets = FXCollections.observableList(MappingSet.getMappingSets(activeOnly));
@@ -236,6 +268,7 @@ public class MappingController {
 			//TODO GUI prompt;
 			mappingSets = FXCollections.observableArrayList();
 		}
-		mappingTableView.setItems(mappingSets);
+		mappingSetTableView.setItems(mappingSets);
 	}
+
 }
