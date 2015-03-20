9ed66320a550d47e8b60a682a98ebe4b2239a7cf
darmbrust <daniel.armbrust.list@gmail.com>
2015-03-20 03:36:37 -0500

    remove unecessary (and wrong) code

--- refex-view/src/main/java/gov/va/isaac/gui/refexViews/dynamicRefexListView/referencedItemsView/DynamicReferencedItemsView.java
+++ refex-view/src/main/java/gov/va/isaac/gui/refexViews/dynamicRefexListView/referencedItemsView/DynamicReferencedItemsView.java
@@ -57,7 +57,7 @@ public class DynamicReferencedItemsView implements PopupViewI
 		title.setPadding(new Insets(5, 5, 5, 5));
 		root_.setTop(title);
 		
-		drv_ = AppContext.getService(DynamicRefexView.class, "DynamicSememeView");
+		drv_ = AppContext.getService(DynamicRefexView.class);
 		root_.setCenter(drv_.getView());
 	}
 	
@@ -78,6 +78,6 @@ public class DynamicReferencedItemsView implements PopupViewI
 		stage.setWidth(800);
 		stage.setHeight(600);
 		stage.show();
-		drv_.setAssemblage(assemblageConcept_.getNid(), null, null, null);
+		drv_.setAssemblage(assemblageConcept_.getNid(), null, null, null, true);
 	}
 }
