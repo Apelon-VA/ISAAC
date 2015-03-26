17bd0ab22789349b9669b9c78f5a4f8e09742e52
darmbrust <daniel.armbrust.list@gmail.com>
2015-03-26 18:29:38 -0500

    Implement the required description search types for mapping.

--- mapping/src/main/java/gov/va/isaac/gui/mapping/data/MappingUtils.java
+++ mapping/src/main/java/gov/va/isaac/gui/mapping/data/MappingUtils.java
@@ -18,17 +18,29 @@
  */
 package gov.va.isaac.gui.mapping.data;
 
+import gov.va.isaac.ExtendedAppContext;
 import gov.va.isaac.constants.MappingConstants;
 import gov.va.isaac.gui.SimpleDisplayConcept;
+import gov.va.isaac.refexDynamic.RefexDynamicUtil;
+import gov.va.isaac.search.CompositeSearchResult;
 import gov.va.isaac.search.SearchHandle;
 import gov.va.isaac.search.SearchHandler;
+import gov.va.isaac.search.SearchResultsIntersectionFilter;
 import gov.va.isaac.util.OTFUtility;
 import gov.va.isaac.util.TaskCompleteCallback;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.UUID;
+import java.util.function.Function;
+import org.ihtsdo.otf.query.lucene.LuceneDescriptionType;
 import org.ihtsdo.otf.tcc.api.concept.ConceptVersionBI;
 import org.ihtsdo.otf.tcc.api.contradiction.ContradictionException;
+import org.ihtsdo.otf.tcc.api.coordinate.ViewCoordinate;
+import org.ihtsdo.otf.tcc.api.description.DescriptionVersionBI;
+import org.ihtsdo.otf.tcc.api.refexDynamic.RefexDynamicChronicleBI;
+import org.ihtsdo.otf.tcc.datastore.BdbTerminologyStore;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -39,7 +51,6 @@ import org.slf4j.LoggerFactory;
  */
 public class MappingUtils
 {
-	//TODO DAN write methods for description search criteria
 	protected static final Logger LOG = LoggerFactory.getLogger(MappingUtils.class);
 	
 	public static List<SimpleDisplayConcept> getStatusConcepts() throws IOException
@@ -82,14 +93,163 @@ public class MappingUtils
 	
 	/**
 	 * Launch a search in a background thread (returns immediately) handing back a handle to the search.
-	 * 
 	 * @param searchString - the query string
 	 * @param callback - (optional) the class instance that desires a callback when the background threaded search completes
-	 * @return
+	 * @param descriptionType - (optional) if provided, only searches within the specified description type
+	 * @param advancedDescriptionType - (optional) if provided, only searches within the specified advanced description type.  
+	 * When this parameter is provided, the descriptionType parameter is ignored.
+	 * @param targetCodeSystemPathNid - (optional) Restrict the results to concepts from the specified path. 
+	 * @param memberOfRefsetNid - (optional) Restrict the results to concepts that are members of the specified refset.
+	 * @param childOfNid - (optional) restrict the results to concepts that are children of the specified concept
+	 * @return - A handle to the running search.
 	 * @throws IOException
 	 */
-	public static SearchHandle search(String searchString, TaskCompleteCallback callback) throws IOException
+	public static SearchHandle search(String searchString, TaskCompleteCallback callback, LuceneDescriptionType descriptionType, 
+			UUID advancedDescriptionType, Integer targetCodeSystemPathNid, Integer memberOfRefsetNid, Integer childOfNid) throws IOException
 	{
-		return SearchHandler.descriptionSearch(searchString, Integer.MAX_VALUE, callback, true);
+		ArrayList<Function<List<CompositeSearchResult>, List<CompositeSearchResult>>> filters = new ArrayList<>();
+		
+		if (targetCodeSystemPathNid != null)
+		{
+			filters.add(new Function<List<CompositeSearchResult>, List<CompositeSearchResult>>()
+			{
+				@Override
+				public List<CompositeSearchResult> apply(List<CompositeSearchResult> t)
+				{
+					ArrayList<CompositeSearchResult> keep = new ArrayList<>();
+					
+					for (CompositeSearchResult csr : t)
+					{
+						if (csr.getContainingConcept().getPathNid() == targetCodeSystemPathNid)
+						{
+							keep.add(csr);
+						}
+					}
+					return keep;
+				}
+			});
+		}
+		
+		if (memberOfRefsetNid != null)
+		{
+			filters.add(new Function<List<CompositeSearchResult>, List<CompositeSearchResult>>()
+			{
+				@Override
+				public List<CompositeSearchResult> apply(List<CompositeSearchResult> t)
+				{
+					try
+					{
+						ArrayList<CompositeSearchResult> keep = new ArrayList<>();
+						HashSet<Integer> refsetMembers = new HashSet<>();
+
+						for(RefexDynamicChronicleBI<?> member : RefexDynamicUtil.readMembers(memberOfRefsetNid, true, null))
+						{
+							refsetMembers.add(member.getReferencedComponentNid());
+						}
+						
+						for (CompositeSearchResult csr : t)
+						{
+							if (refsetMembers.contains(csr.getContainingConcept().getNid()))
+							{
+								keep.add(csr);
+							}
+						}
+						return keep;
+					}
+					catch (Exception e)
+					{
+						throw new RuntimeException(e);
+					}
+				}
+			});
+		}
+		
+		if (childOfNid != null)
+		{
+			filters.add(new Function<List<CompositeSearchResult>, List<CompositeSearchResult>>()
+			{
+				@Override
+				public List<CompositeSearchResult> apply(List<CompositeSearchResult> t)
+				{
+					try
+					{
+						ArrayList<CompositeSearchResult> keep = new ArrayList<>();
+						BdbTerminologyStore ds = ExtendedAppContext.getDataStore();
+						ViewCoordinate vc = OTFUtility.getViewCoordinate();
+						
+						for (CompositeSearchResult csr : t)
+						{
+							if (ds.isChildOf(csr.getContainingConcept().getNid(), childOfNid, vc))
+							{
+								keep.add(csr);
+							}
+						}
+						return keep;
+					}
+					catch (IOException | ContradictionException e)
+					{
+						throw new RuntimeException(e);
+					}
+				}
+			});
+		}
+		
+		SearchResultsIntersectionFilter filterSet = (filters.size() > 0 ? new SearchResultsIntersectionFilter(filters) : null);
+		
+		if (descriptionType == null && advancedDescriptionType == null)
+		{
+			return SearchHandler.descriptionSearch(searchString, 500, false, (UUID)null, callback, null, filterSet, null, true);
+		}
+		else if (advancedDescriptionType != null)
+		{
+			return SearchHandler.descriptionSearch(searchString, 500, false, advancedDescriptionType, callback, null, filterSet, null, true);
+		}
+		else if (descriptionType != null)
+		{
+			return SearchHandler.descriptionSearch(searchString, 500, false, descriptionType, callback, null, filterSet, null, true);
+		}
+		else
+		{
+			throw new RuntimeException("Logic failure!");
+		}
+	}
+	
+	/**
+	 * Launch a search in a background thread (returns immediately) handing back a handle to the search.
+	 * @param sourceConceptNid - the source concept of the map - the descriptions of this concept will be used to create a search
+	 * @param callback - (optional) the class instance that desires a callback when the background threaded search completes
+	 * @param descriptionType - (optional) if provided, only searches within the specified description type
+	 * @param advancedDescriptionType - (optional) if provided, only searches within the specified advanced description type.  
+	 * When this parameter is provided, the descriptionType parameter is ignored.
+	 * @param targetCodeSystemPathNid - (optional) Restrict the results to concepts from the specified path. 
+	 * @param memberOfRefsetNid - (optional) Restrict the results to concepts that are members of the specified refset.
+	 * @param childOfNid - (optional) restrict the results to concepts that are children of the specified concept
+	 * @return - A handle to the running search.
+	 * @throws IOException
+	 */
+	public static SearchHandle search(int sourceConceptNid, TaskCompleteCallback callback, LuceneDescriptionType descriptionType, 
+			UUID advancedDescriptionType, Integer targetCodeSystemPathNid, Integer memberOfRefsetNid, Integer childOfNid) throws IOException
+	{
+		StringBuilder searchString;
+		try
+		{
+			searchString = new StringBuilder();
+			
+			ConceptVersionBI cv = OTFUtility.getConceptVersion(sourceConceptNid);
+			
+			for (DescriptionVersionBI<?> desc : cv.getDescriptionsActive())
+			{
+				//brackets are somewhat common, and choke the query parser
+				searchString.append(desc.getText().replaceAll("\\[", "\\\\[").replaceAll("\\]", "\\\\]"));
+				searchString.append(" ");
+			}
+		}
+		catch (ContradictionException e)
+		{
+			LOG.error("Unexpected error", e);
+			throw new IOException(e);
+		}
+		
+		return search(searchString.toString(), callback, descriptionType, advancedDescriptionType, targetCodeSystemPathNid, memberOfRefsetNid, childOfNid);
 	}
 }
---
+++ otf-util/src/main/java/gov/va/isaac/refexDynamic/RefexDynamicUtil.java
@@ -0,0 +1,106 @@
+/**
+ * Copyright Notice
+ *
+ * This is a work of the U.S. Government and is not subject to copyright 
+ * protection in the United States. Foreign copyrights may apply.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package gov.va.isaac.refexDynamic;
+
+import gov.va.isaac.AppContext;
+import gov.va.isaac.ExtendedAppContext;
+import gov.va.isaac.util.OTFUtility;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import javafx.application.Platform;
+import javafx.scene.control.ProgressIndicator;
+import org.ihtsdo.otf.query.lucene.LuceneDynamicRefexIndexer;
+import org.ihtsdo.otf.query.lucene.LuceneDynamicRefexIndexerConfiguration;
+import org.ihtsdo.otf.tcc.api.concept.ConceptVersionBI;
+import org.ihtsdo.otf.tcc.api.refexDynamic.RefexDynamicChronicleBI;
+import org.ihtsdo.otf.tcc.model.index.service.SearchResult;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * {@link RefexDynamicUtil}
+ *
+ * @author <a href="mailto:daniel.armbrust.list@gmail.com">Dan Armbrust</a> 
+ */
+public class RefexDynamicUtil
+{
+	private static Logger logger_ = LoggerFactory.getLogger(RefexDynamicUtil.class);
+	
+	@SuppressWarnings("unchecked")
+	public static Collection<RefexDynamicChronicleBI<?>> readMembers(int dynamicRefexConceptNid, boolean allowUnindexedScan, ProgressIndicator progress) 
+			throws Exception
+	{
+		Collection<RefexDynamicChronicleBI<?>> refexMembers;
+		
+		ConceptVersionBI assemblageConceptFull = OTFUtility.getConceptVersion(dynamicRefexConceptNid);
+		if (assemblageConceptFull.isAnnotationStyleRefex())
+		{
+			refexMembers = new ArrayList<>();
+			
+			if (LuceneDynamicRefexIndexerConfiguration.isAssemblageIndexed(assemblageConceptFull.getNid()))
+			{
+				logger_.debug("Using index to read annotation style refex members");
+				if (progress != null)
+				{
+					Platform.runLater(() ->
+					{
+						progress.setProgress(-1);
+					});
+				}
+				
+				LuceneDynamicRefexIndexer ldri = AppContext.getService(LuceneDynamicRefexIndexer.class);
+				
+				List<SearchResult> results = ldri.queryAssemblageUsage(assemblageConceptFull.getNid(), Integer.MAX_VALUE, Long.MIN_VALUE);
+				for (SearchResult sr : results)
+				{
+					refexMembers.add((RefexDynamicChronicleBI<?>)ExtendedAppContext.getDataStore().getComponent(sr.getNid()));
+				}
+			}
+			else
+			{
+				if (allowUnindexedScan)
+				{
+					logger_.debug("Using full database scan to read annotation style refex members");
+					RefexAnnotationSearcher processor = new RefexAnnotationSearcher((refex) -> 
+					{
+						if (refex.getAssemblageNid() == assemblageConceptFull.getConceptNid())
+						{
+							return true;
+						}
+						return false;
+					}, progress);
+
+					ExtendedAppContext.getDataStore().iterateConceptDataInParallel(processor);
+					refexMembers.addAll(processor.getResults());
+				}
+				else
+				{
+					throw new RuntimeException("No index available and full scan not allowed!");
+				}
+			}
+		}
+		else
+		{
+			logger_.debug("Reading member style refex members");
+			refexMembers = (Collection<RefexDynamicChronicleBI<?>>)assemblageConceptFull.getRefsetDynamicMembers();
+		}
+		return refexMembers;
+	}
+}
