d9fdfcff8c60b0f83121ce82e133799f1f699945
dtriglianos <dtriglianos@apelon.com>
2015-04-30 12:55:10 -0400

    Added note

--- gui-util/src/main/java/gov/va/isaac/util/SearchStringProcessor.java
+++ gui-util/src/main/java/gov/va/isaac/util/SearchStringProcessor.java
@@ -9,6 +9,7 @@ public class SearchStringProcessor {
     public static final String operatorsRegEx = "\\+|\\-|\\*|\\/|<|>|=|\\^|~";
     public static final String parensRegEx = "\\(|\\)|\\{|\\}|\\[|\\]";
 
+    // Note: \xc2\xa0 is non-breaking space
 	public static final String nonPrintableRegEx = 
 			"\\x00|\\x01|\\x02|\\x03|\\x04|\\x05|\\x06|\\x07|\\x08|\\x09|\\x0a|\\x0b|\\x0c|\\x0d|\\x0e|\\x0f|" +
 			"\\x10|\\x11|\\x12|\\x13|\\x14|\\x15|\\x16|\\x17|\\x18|\\x19|\\x1a|\\x1b|\\x1c|\\x1d|\\x1e|\\x1f|" +
